f961731f226590573fbdf5ae6111ab3b101ced4f
Tim Schlechter <timschlechter@gmail.com>
2016-08-02 09:04:31 +0200

    Simplified HttpApiClient

--- src/CQ.HttpApi.Client/HttpApiClient.cs
+++ src/CQ.HttpApi.Client/HttpApiClient.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using CQ.HttpApi;
 using CQ.HttpApi.JsonSerialization;
@@ -17,19 +15,19 @@ namespace CQ.Client
         public HttpApiClient(string rootUrl, HttpApiConfig config = null)
         {
             _rootUrl = rootUrl;
+
             _config = config ?? HttpApiConfig.Default;
+            _config.JsonSerializer = _config.JsonSerializer ?? HttpApiConfig.Default.JsonSerializer;
             _config.CommandRouteResolver = _config.CommandRouteResolver ?? HttpApiConfig.Default.CommandRouteResolver;
             _config.QueryRouteResolver = _config.QueryRouteResolver ?? HttpApiConfig.Default.QueryRouteResolver;
-            _config.JsonSerializer = _config.JsonSerializer ?? new SimpleJsonSerializer();
+            _config.CommandGroupKeyResolver = _config.CommandGroupKeyResolver ?? HttpApiConfig.Default.CommandGroupKeyResolver;
+            _config.QueryGroupKeyResolver = _config.QueryGroupKeyResolver ?? HttpApiConfig.Default.QueryGroupKeyResolver;
         }
 
         public Task ExecuteCommand<TCommand>(TCommand command)
         {
             var path = _config.CommandRouteResolver.ResolvePath(command);
-            var req = new RestRequest(path)
-            {
-                Method = Method.POST
-            };
+            var req = new RestRequest(path, Method.POST);
 
             req.AddJsonBody(command);
 
@@ -40,13 +38,10 @@ namespace CQ.Client
         public Task<TResult> ExecuteQuery<TQuery, TResult>(TQuery query) where TQuery : IQuery<TResult>
         {
             var path = _config.QueryRouteResolver.ResolvePath(query);
-            var req = new RestRequest(path)
-            {
-                Method = Method.GET
-            };
+            var req = new RestRequest(path, Method.GET);
 
             var parameters = ObjectHelper.Flatten(query)
-                .SelectMany(group => 
+                .SelectMany(group =>
                     group.Select(value => new Parameter
                     {
                         Name = group.Key,
@@ -59,50 +54,22 @@ namespace CQ.Client
             return ExecuteRequest<TResult>(req);
         }
 
-        public static IDictionary<string, object> ToDictionary(object value)
-        {
-            return value.GetType()
-                .GetProperties()
-                .Select(pi => new {pi.Name, Value = pi.GetValue(value, null)})
-                .Union(
-                    value.GetType()
-                        .GetFields()
-                        .Select(fi => new {fi.Name, Value = fi.GetValue(value)})
-                )
-                .ToDictionary(ks => ks.Name, vs => vs.Value);
-        }
-
         protected virtual Task<TResult> ExecuteRequest<TResult>(IRestRequest req)
         {
             var client = new RestClient(_rootUrl);
 
-            return client.ExecuteTaskAsync(req)
-                .ContinueWith(task =>
+            return client.ExecuteTaskAsync(req).ContinueWith(task =>
+            {
+                var response = task.Result;
+
+                var status = (int) response.StatusCode;
+                if (status < 200 && status >= 400)
                 {
-                    var response = task.Result;
+                    throw new HttpApiException(response.StatusDescription) {StatusCode = response.StatusCode};
+                }
 
-                    var status = (int) response.StatusCode;
-                    if (status < 200 && status >= 400)
-                    {
-                        throw new HttpApiException(response.StatusDescription) {StatusCode = response.StatusCode};
-                    }
-
-                    using (var stream = new MemoryStream())
-                    {
-                        using (var writer = new StreamWriter(stream))
-                        {
-                            writer.Write(response.Content);
-                            writer.Flush();
-                            stream.Position = 0;
-
-                            return (TResult) _config.JsonSerializer.Deserialize(stream, typeof (TResult));
-                        }
-                    }
-                });
+                return _config.JsonSerializer.Deserialize<TResult>(response.Content);
+            });
         }
-    }
-
-    public class CommandResult
-    {
     }
 }
--- src/CQ.HttpApi/JsonSerialization/JsonSerializerExtensions.cs
+++ src/CQ.HttpApi/JsonSerialization/JsonSerializerExtensions.cs
@@ -28,5 +28,30 @@ namespace CQ.HttpApi.JsonSerialization
                 }
             }
         }
+
+        public static object Deserialize(this IJsonSerializer serializer, string json, Type type)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(json);
+                    writer.Flush();
+                    stream.Position = 0;
+
+                    return serializer.Deserialize(stream, type);
+                }
+            }
+        }
+
+        public static T Deserialize<T>(this IJsonSerializer serializer, string json)
+        {
+            return (T)serializer.Deserialize(json, typeof (T));
+        }
+
+        public static T Deserialize<T>(this IJsonSerializer serializer, Stream stream)
+        {
+            return (T)serializer.Deserialize(stream, typeof(T));
+        }
     }
 }
